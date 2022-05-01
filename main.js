3d08940f8aac0ffe71709b83362bc094614af729
Chris Choi <chrischoi3131@gmail.com>
2022-05-01 16:55:03 -0700

    Add animation of navbar while scrolling

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let lastScrollY = 0;
+const navbar = document.querySelector('#navbar')
+const heightNavbar = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',()=> {
+    if(lastScrollY < window.scrollY){
+      if(heightNavbar < window.scrollY){
+        navbar.classList.add('navbar__hide');
+        lastScrollY = window.scrollY;
+      }
+    }else if(lastScrollY > window.scrollY){
+      navbar.classList.remove('navbar__hide');
+      lastScrollY = window.scrollY;
+    }
+});
