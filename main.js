1b1d9d6940d614c0aa4268d6cba1cd56fe71489a
Chris Choi <chrischoi3131@gmail.com>
2022-05-01 17:28:28 -0700

    Add Hover animation on navitem

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const heightNavbar = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=> {
     if(lastScrollY < window.scrollY){
       if(heightNavbar < window.scrollY){
+        
         navbar.classList.add('navbar__hide');
         lastScrollY = window.scrollY;
       }
     }else if(lastScrollY > window.scrollY){
       navbar.classList.remove('navbar__hide');
+      
       lastScrollY = window.scrollY;
     }
 });
