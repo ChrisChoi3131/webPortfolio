e219712b287c6b5b7709c6c4457441aec60b55d5
Chris Choi <chrischoi3131@gmail.com>
2022-05-10 01:42:14 -0700

    Modify footer color change when it is in Section'Contact'

--- main.js
+++ main.js
@@ -8,9 +8,8 @@ const navbarMenu = document.querySelector('.navbar__menu');
 const heightNavbar = navbar.getBoundingClientRect().height;
 const sections = document.querySelectorAll('section');
 const footerRight = document.querySelector('#footerRight');
+const footerLeft = document.querySelector('#footerLeft');
 const switchFixHeader = document.querySelector('#switchFixHeader');
-let textFooterRight = 'Home';
-footerRight.innerHTML = textFooterRight;
 document.addEventListener('scroll', () => {
   if (isFixedHeader === true) return;
   if (lastScrollY < window.scrollY) {
@@ -23,7 +22,19 @@ document.addEventListener('scroll', () => {
     lastScrollY = window.scrollY;
   }
 });
-
+document.addEventListener('scroll', () => {
+  if (window.innerHeight - sections[sections.length - 1].getBoundingClientRect().y > 30) {
+    footerRight.style.color = 'white';
+    for (const child of footerLeft.children) {
+      child.style.color = 'white';
+    }
+  } else {
+    footerRight.style.color = 'var(--color-pink)';
+    for (const child of footerLeft.children) {
+      child.style.color = 'var(--color-grey)';
+    }
+  }
+});
 switchFixHeader.addEventListener('click', e => {
   if (e.target.tagName !== 'INPUT') return;
   if (e.target.checked) isFixedHeader = true;
@@ -40,18 +51,45 @@ navbarMenu.addEventListener('click', e => {
   const element = document.getElementById(target.dataset.link);
   element.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
+const sectionsId = Array.from(sections).map(section => section.id);
+const navItems = new Map(
+  sectionsId.map((sectionId, idx) => [
+    sectionId,
+    { idx, element: document.querySelector(`[data-link="${sectionId}"]`) },
+  ])
+);
 
 const callbackObserver = entries => {
-  entries.forEach(entry => {
-    const text = entry.target.id.split('');
-    text[0] = text[0].toUpperCase();
-    if (entry.isIntersecting) {
-      footerRight.innerHTML = text.join('');
-    }
-  });
+  if (entries.length > 1) {
+    const currEntries = [];
+    entries.forEach(entry => {
+      const currentRatio = entry.intersectionRatio;
+      const target = entry.target.id;
+      if (currentRatio > 0) currEntries.push({ target, currentRatio, idx: navItems.get(target).idx });
+    });
+    console.log(currEntries);
+    currEntries.sort((a, b) => {
+      if (b.currentRatio > a.currentRatio) {
+        return 1;
+      } else if (b.currentRatio < a.currentRatio) {
+        return -1;
+      } else {
+        if (a.idx < b.idx) {
+          return -1;
+        } else {
+          return 1;
+        }
+      }
+    });
+    footerRight.innerText = currEntries[0].target;
+  } else {
+    const entry = entries[0];
+    const currentRatio = entry.intersectionRatio;
+    const target = entry.target.id;
+  }
 };
 const optionsObserver = {
-  threshold: 0.6,
+  threshold: 1,
 };
 const observer = new IntersectionObserver(callbackObserver, optionsObserver);
 sections.forEach(section => observer.observe(section));
